dda38ca37bb5253c75bb3919f336426914d7f03a
Samuel Oliveira <samuelcontatodev@gmail.com>
2022-08-23 11:49:19 -0300

    jogo funcionando

--- scripts/game.js
+++ scripts/game.js
@@ -1,22 +1,41 @@
-const canvas = document.getElementById('box')
-const context = canvas.getContext("2d")
+const cvs = document.getElementById("box")
+const ctx = cvs.getContext("2d")
+const scoreElement = document.getElementById("score")
+const speedElement = document.getElementById("speed")
 
-const score = document.getElementById("score")
-const speed = document.getElementById("speed")
-
-const rows = 20
-const columns = 10
-const pixels = 30
+const ROW = 20
+const COL = 10
+const SQ = 30
 const defaultColor = "#111111"
-const defaultBorder = "rgba(255, 255, 255, 0.1)"
+const defaultBorder = "rgba(255,255,255,0.1)"
+
+let canMove = true
+let speed = 500
+let dropStart = Date.now()
+let score = 0
 
 let board = []
-
-for (let currentRow = 0; currentRow < rows; currentRow++) {
+for (let currentRow = 0; currentRow < ROW; currentRow++) {
     board[currentRow] = []
-    for (let currentCol = 0; currentCol < columns; currentCol++) {
+    for(let currentCol = 0; currentCol < COL; currentCol++) {
         board[currentRow][currentCol] = defaultColor
     }
 }
 
 drawBoard()
+
+const PIECES = [
+    [Z,'red'],
+    [S,'green'],
+    [T,'yellow'],
+    [O,'blue'],
+    [L,'purple'],
+    [I,'cyan'],
+    [J,'orange'],
+]
+
+let piece = randomPiece()
+
+drop()
+
+document.addEventListener("keydown", CONTROL)
